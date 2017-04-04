0aa221166059bed5ab09d45cd50434428989e9bb
ocuweb <jfuente@ocu.org>
2017-04-04 15:46:27 +0200

    Update scripts.js

--- compare/scripts.js
+++ compare/scripts.js
@@ -1,6 +1,13 @@
 alert("online guay2")
 (function() {
   //fix PS home
-  var divs=document.querySelectorAll('[data-selector="aline-content-link"]');for (var div of divs) {if (div.href.indexOf("/test/")>=1){}else{div.style.display = "none";}}
-
+  var q='[data-selector="aline-content-link"]';
+  alert(q)
+  var divs=document.querySelectorAll(q);
+  for (var div of divs) {
+    if (div.href.indexOf("/test/")>=1){
+    }else{
+      div.style.display = "none";
+    }
+  }
 })();
